d210aad4dd5a050111cdaeaa314731955888dc0a
rhamadhany <r6282256211942@gmail.com>
2025-11-06 16:59:39 +0800

    update

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/home/rhamadhany/NewDisk/flutter"
-export "FLUTTER_APPLICATION_PATH=/home/rhamadhany/NewDisk/projek-flutter/installed_apps/example"
+export "FLUTTER_ROOT=/media/rhamadhany/NewDisk/flutter"
+export "FLUTTER_APPLICATION_PATH=/media/rhamadhany/NewDisk/projek-flutter/installed_apps/example"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
